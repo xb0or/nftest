2e81f7a45505bcf578e0363b0f7205ec39142bf6
xb0or <57210034+xb0or@users.noreply.github.com>
2021-03-04 21:09:03 +0800

    Update netflix.sh

--- netflix.sh
+++ netflix.sh
@@ -24,7 +24,7 @@ function test_ipv4() {
         echo -e "\033[33m你的IP可以打开Netflix 但是仅解锁自制剧\033[0m";
         return;
     fi
-    
+    #奈飞IPV4区域测试
     region=`tr [:lower:] [:upper:] <<< $(curl -4is "https://www.netflix.com/title/80018499" 2>&1 | sed -n '8p' | awk '{print $2}' | cut -d '/' -f4 | cut -d '-' -f1)` ;
 
     if [[ "$region" == *"INDEX"* ]];then
@@ -60,9 +60,8 @@ function test_ipv6() {
         echo -e "\033[33m你的IP可以打开Netflix 但是仅解锁自制剧\033[0m";
         return;
     fi
-    
-    region=`tr [:lower:] [:upper:] <<< $(curl -6is "https://www.netflix.com/title/80018499" 2>&1 | sed -n '8p' | awk '{print $2}' | cut -d '/' -f4 | cut -d '-' -f1)` ;
-    
+    #奈飞IPV4区域测试
+    region=`tr [:lower:] [:upper:] <<< $(curl -6is "https://www.netflix.com/title/80018499" 2>&1 | sed -n '8p' | awk '{print $2}' | cut -d '/' -f4 | cut -d '-' -f1)` ;  
     if [[ "$region" == *"INDEX"* ]];then
        region="US";
     fi
@@ -81,21 +80,57 @@ if [ $? -ne 0 ];then
     exit;
 fi
 
-echo -e "\033[36m Netflix测试脚本 \033[0m"
+
+   #油管IPV4区域测试
+   area=$(curl -4 -s https://www.youtube.com/red | sed 's/,/\n/g' | grep countryCode | cut -d '"' -f4)
+    if [[ "$region" == ""]];then
+       region="US";
+    fi
+yt_ipv4(){
+   #油管IPV4区域测试
+   area=$(curl -6 -s https://www.youtube.com/red | sed 's/,/\n/g' | grep countryCode | cut -d '"' -f4)
+    if [[ "$region" == ""]];then
+       area="US";
+    fi
+echo -e "\033[32m 你的油管区域: ${area}\033[0m";
+}
+yt_ipv6(){
+   #油管IPV6区域测试
+   area=$(curl -6 -s https://www.youtube.com/red | sed 's/,/\n/g' | grep countryCode | cut -d '"' -f4)
+    if [[ "$region" == ""]];then
+       area="US";
+    fi
+echo -e "\033[32m 你的油管区域: ${area}\033[0m";
+}
 
 
-echo " ** 正在测试IPv4解锁情况";
+
+
+
+
+
+
+#目录
+
+echo -e "\033[36m 测试脚本 V2.0 \033[0m"
+echo -e "\033[36m GitHub：https://github.com/xb0or/nftest \033[0m"
+echo -e "\033[36m bash <(curl -sSL "https://raw.githubusercontent.com/xb0or/nftest/main/netflix.sh") \033[0m"
+
+echo " ** 正在测试 IPv4 解锁情况";
 check4=`ping 1.1.1.1 -c 1 2>&1`;
 if [[ "$check4" != *"unreachable"* ]] && [[ "$check4" != *"Unreachable"* ]];then
-    test_ipv4;
-else
     echo -e "\033[34m当前主机不支持IPv4,跳过...\033[0m";
+else
+test_ipv4
+yt_ipv4
 fi
 
-echo " ** 正在测试IPv6解锁情况";
+echo " ** 正在测试 IPv6 解锁情况";
 check6=`ping6 240c::6666 -c 1 2>&1`;
 if [[ "$check6" != *"unreachable"* ]] && [[ "$check6" != *"Unreachable"* ]];then
-    test_ipv6;
+echo -e "\033[34m当前主机不支持IPv6,跳过...\033[0m";    
+
 else
-    echo -e "\033[34m当前主机不支持IPv6,跳过...\033[0m";
+    test_ipv6
+    yt_ipv6
 fi
